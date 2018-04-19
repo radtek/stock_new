8fe39c281282787631bc37086d906a83c45ec086
oscar <vsoscar0115@gmail.com>
2018-04-19 23:53:12 +0800

    tmp

---
+++ CommonUrl.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class CommonUrl : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        HyperLink hy = new HyperLink();
+
+        hy.Text = "台指期貨日盤走勢";
+        hy.NavigateUrl = "http://mis.twse.com.tw/stock/futures.jsp";
+        hy.Target = "_Blank";
+
+        Page.Controls.Add(hy);
+
+    }
+}
+
