dd788dbfd25911f6879e641a988a4ca50e17ceee
oscar <vsoscar0115@gmail.com>
2018-10-30 15:34:20 +0800

    added 加減密網址

--- Default2.aspx
+++ Default2.aspx
@@ -5,21 +5,65 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml" >
 <head runat="server">
-    <title>未命名頁面</title>
+    <title>加密解密TEST</title>
    <script type="text/javascript"> 
    var testArray = new Array;  
    testArray=["dddd", "abc", "wer"]; 
    
    
    
-   $( "#TextBox1" ).autocomplete({ source: testArray});
+   $( "#TextBox2" ).autocomplete({ source: testArray});
    
    </script>
 </head>
 <body>
     <form id="form1" runat="server" method="post">
     <div>
-        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></div>
+        <br />
+        加密字串<br />
+        <br />
+        <asp:TextBox ID="TextBox2" runat="server" Width="308px"></asp:TextBox><br />
+        加密KEY<br />
+        <asp:TextBox ID="TextBox3" runat="server" Width="173px"></asp:TextBox><br />
+        <br />
+        加密IV<br />
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox><br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="執行加密" Width="83px" OnClick="Button1_Click" /><br />
+        <br />
+        加密結果<br />
+        <asp:TextBox ID="TextBox1" runat="server" Width="296px"></asp:TextBox><br />
+        <br />
+        <br />
+        <asp:HyperLink ID="HyperLink1" runat="server">HyperLink</asp:HyperLink>
+        &nbsp; &nbsp; &nbsp; &nbsp;
+        <asp:HyperLink ID="HyperLink2" runat="server">HyperLink</asp:HyperLink><br />
+        ******************************************************<br />
+        <br />
+        解密字串<br />
+        <asp:TextBox ID="TextBox5" runat="server" Width="296px"></asp:TextBox><br />
+        <br />
+        解密KEY<br />
+        <asp:TextBox ID="TextBox6" runat="server" Width="173px"></asp:TextBox><br />
+        <br />
+        解密IV<br />
+        <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox><br />
+        <br />
+        <asp:Button ID="Button2" runat="server" Text="執行解密" OnClick="Button2_Click" /><br />
+        <br />
+        解密結果<br />
+        <asp:TextBox ID="TextBox8" runat="server" Width="296px"></asp:TextBox><br />
+        <br />
+        <asp:HyperLink ID="HyperLink3" runat="server">欲加密網址</asp:HyperLink><br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    </div>
     </form>
 </body>
 </html>
--- Default2.aspx.cs
+++ Default2.aspx.cs
@@ -12,6 +12,7 @@ using System.IO;
 using System.Net;
 using HtmlAgilityPack;
 using System.Text;
+using System.Security.Cryptography;
 
 
 public partial class Default2 : System.Web.UI.Page
@@ -22,25 +23,164 @@ public partial class Default2 : System.Web.UI.Page
     string today_detail = DateTime.Now.AddDays(+0).ToString("yyyyMMddHHmmss");
     protected void Page_Load(object sender, EventArgs e)
     {
+        string sourceFile = Server.MapPath(".") + "\\a.txt";
+
+        string encryptFile = Server.MapPath(".") + "\\b.txt";
+
+        string decryptFile = Server.MapPath(".") + "\\c.txt";
+        TextBox1.Visible = false;
+        TextBox5.Visible = false;
+        if (!IsPostBack)
+        {
+
+            TextBox2.Text = "https://tw.yahoo.com?userid=jolina&flag=Y";
+            TextBox3.Text = "12345678";
+            TextBox6.Text = "12345678";
+
+            TextBox4.Text = "87654321";
+            TextBox7.Text = "87654321";
+
+
+            HyperLink1.NavigateUrl = "http://vsoscar007.ddns.net:8080/stocK_new/" + "a.txt";
+            HyperLink1.Text = "欲加密網址";
+            HyperLink1.Target = "_blank";
+
+            HyperLink2.NavigateUrl = "http://vsoscar007.ddns.net:8080/stocK_new/" + "b.txt";
+            HyperLink2.Text = "加密後網址";
+            HyperLink2.Target = "_blank";
+
+            HyperLink3.NavigateUrl = "http://vsoscar007.ddns.net:8080/stocK_new/" + "c.txt";
+            HyperLink3.Text = "解密後網址";
+            HyperLink3.Target = "_blank";
+
         
-        //target=Check_function_Fund_price("");
-        //Response.Write(target);
-    
-        //*delete_log_file(Server.MapPath(".") + "\\RUN_LOG\\", "*.exe", -30);
-        //string aaa = "M:\\report\\Stock_new\\wkhtmltopdf\\bin\\wkhtmltopdf.exe" ;
-        string tool = Server.MapPath(".")  + "\\wkhtmltopdf\\bin\\wkhtmltopdf.exe";
-
-        string website = "http://www.wantgoo.com/globalfinance.aspx";
+        }
 
         
-        func.CreatePDF(website, tool, Server.MapPath(".") + "\\File\\" + today_detail + ".pdf");
+        
+       
+        
 
-        //CreatePDF(website, tool, Server.MapPath(".") + "\\File\\" + today_detail + ".pdf");
+        //desDecryptFile(encryptFile, decryptFile,"12345678","87654321");
+
 
       
     }
 
 
+    public static void desEncryptFile(string sourceFile, string encryptFile,string skey,string siv)
+    {
+
+        if (string.IsNullOrEmpty(sourceFile) || string.IsNullOrEmpty(encryptFile))
+        {
+
+            return;
+
+        }
+
+        if (!File.Exists(sourceFile))
+        {
+
+            return;
+
+        }
+
+
+        DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+        byte[] key = Encoding.ASCII.GetBytes(skey);
+
+        byte[] iv = Encoding.ASCII.GetBytes(siv);
+
+
+
+        des.Key = key;
+
+        des.IV = iv;
+        //IV屬性是的用來取得或設定對稱算法的初始化向量;即，加解密雙方需要同樣的IV向量
+
+
+        using (FileStream sourceStream = new FileStream(sourceFile, FileMode.Open, FileAccess.Read))
+
+        using (FileStream encryptStream = new FileStream(encryptFile, FileMode.Create, FileAccess.Write))
+        {
+
+            //檔E案!N加D[密OK
+
+            byte[] dataByteArray = new byte[sourceStream.Length];
+
+            sourceStream.Read(dataByteArray, 0, dataByteArray.Length);
+
+
+
+            using (CryptoStream cs = new CryptoStream(encryptStream, des.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+
+                cs.Write(dataByteArray, 0, dataByteArray.Length);
+
+                cs.FlushFinalBlock();
+
+            }
+
+        }
+
+    }
+
+
+    public static void desDecryptFile(string encryptFile, string decryptFile,string skey,string siv)
+    {
+
+        if (string.IsNullOrEmpty(encryptFile) || string.IsNullOrEmpty(decryptFile))
+        {
+
+            return;
+
+        }
+
+        if (!File.Exists(encryptFile))
+        {
+
+            return;
+
+        }
+
+
+
+        DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+        byte[] key = Encoding.ASCII.GetBytes(skey);
+
+        byte[] iv = Encoding.ASCII.GetBytes(siv);
+
+
+        des.Key = key;
+
+        des.IV = iv;
+        //IV屬性是的用來取得或設定對稱算法的初始化向量;即，加解密雙方需要同樣的IV向量
+
+
+
+        using (FileStream encryptStream = new FileStream(encryptFile, FileMode.Open, FileAccess.Read))
+
+        using (FileStream decryptStream = new FileStream(decryptFile, FileMode.Create, FileAccess.Write))
+        {
+
+            byte[] dataByteArray = new byte[encryptStream.Length];
+
+            encryptStream.Read(dataByteArray, 0, dataByteArray.Length);
+
+            using (CryptoStream cs = new CryptoStream(decryptStream, des.CreateDecryptor(), CryptoStreamMode.Write))
+            {
+
+                cs.Write(dataByteArray, 0, dataByteArray.Length);
+
+                cs.FlushFinalBlock();
+
+            }
+
+        }
+
+    }
 
     //public static void CreatePDF(string html_Path, string tool_path,string SavePath)
     //{
@@ -178,5 +318,122 @@ public partial class Default2 : System.Web.UI.Page
 
 
 
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string sourceFile = Server.MapPath(".") + "\\a.txt";
+
+        string encryptFile = Server.MapPath(".") + "\\b.txt";
+
+        //FileInfo fi;//宣告檔案 
+        //fi = new FileInfo(Server.MapPath(".") + "\\a.txt");
+        //fi.Delete();
+
+        StreamWriter sw;
+        //UTF8Encoding utf8 = new UTF8Encoding();
+
+        // sw.Encoding = System.Text.UTF8Encoding();
+        //StreamWriter sw = new StreamWriter(Encoding.UTF8);
+
+        DirectoryInfo di;//宣告目錄 
+        FileInfo fi;//宣告檔案 
+        //string program_name1 = program_name;
+        //di = new DirectoryInfo(Server.MapPath(".") + "\\RUN_LOG\\" ); //DateTime.Now.ToString("yyyyMMdd") 
+        di = new DirectoryInfo(Server.MapPath(".")); //DateTime.Now.ToString("yyyyMMdd") 
+        //fi = new FileInfo(Server.MapPath(".") + "\\RUN_LOG\\" + DateTime.Now.ToString("yyyyMMdd") + "\\" + DateTime.Now.ToString("yyyyMMdd") + ".log");
+        fi = new FileInfo(Server.MapPath(".") + "\\a.txt");
+        fi.Delete();
+        //if (!di.Exists)
+        //{
+        //    di.Create();//目錄不存在 產生目錄 
+        //}
+        if (fi.Exists == true)
+        {
+            fi.Delete();
+           
+          
+            //檔案存在 寫檔案 
+            //sw = File.AppendText(Server.MapPath(".") + "\\RUN_LOG\\" + DateTime.Now.ToString("yyyyMMdd") + ".log");
+            sw = File.AppendText(Server.MapPath(".") + "\\a.txt");
+        }
+        else
+        {
+            sw = fi.CreateText(); //檔案不存在 產生檔案 
+        }
+
+        sw.WriteLine(TextBox2.Text);
+       
+        //byte[] encodebyte = utf8.GetBytes(sw);
+        //string decodestring = utf8.GetString(sw);
+        sw.Close();
+       
+
+        desEncryptFile(sourceFile, encryptFile, TextBox3.Text, TextBox4.Text);
+
+        ArrayList al = new ArrayList();
+
+        al=func.FileToArray(Server.MapPath(".") + "\\b.txt");
+        TextBox1.Text = al[0].ToString();
+
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+
+       
+
+        string encryptFile = Server.MapPath(".") + "\\b.txt";
+
+        string decryptFile = Server.MapPath(".") + "\\c.txt";
+
+        //#region MyRegion
+        //StreamWriter sw;
+        ////UTF8Encoding utf8 = new UTF8Encoding();
+
+        //// sw.Encoding = System.Text.UTF8Encoding();
+        ////StreamWriter sw = new StreamWriter(Encoding.UTF8);
+
+        //DirectoryInfo di;//宣告目錄 
+        //FileInfo fi;//宣告檔案 
+        ////string program_name1 = program_name;
+        ////di = new DirectoryInfo(Server.MapPath(".") + "\\RUN_LOG\\" ); //DateTime.Now.ToString("yyyyMMdd") 
+        //di = new DirectoryInfo(Server.MapPath(".")); //DateTime.Now.ToString("yyyyMMdd") 
+        ////fi = new FileInfo(Server.MapPath(".") + "\\RUN_LOG\\" + DateTime.Now.ToString("yyyyMMdd") + "\\" + DateTime.Now.ToString("yyyyMMdd") + ".log");
+        //fi = new FileInfo(Server.MapPath(".") + "\\b.txt");
+        //fi.Delete();
+        ////if (!di.Exists)
+        ////{
+        ////    di.Create();//目錄不存在 產生目錄 
+        ////}
+        //if (fi.Exists == true)
+        //{
+        //    fi.Delete();
+
+
+        //    //檔案存在 寫檔案 
+        //    //sw = File.AppendText(Server.MapPath(".") + "\\RUN_LOG\\" + DateTime.Now.ToString("yyyyMMdd") + ".log");
+        //    sw = File.AppendText(Server.MapPath(".") + "\\b.txt");
+        //}
+        //else
+        //{
+        //    sw = fi.CreateText(); //檔案不存在 產生檔案 
+        //}
+
+        //sw.WriteLine(TextBox5.Text);
+
+        ////byte[] encodebyte = utf8.GetBytes(sw);
+        ////string decodestring = utf8.GetString(sw);
+        //sw.Close();
+        //#endregion
+        
+        
+        
+        desDecryptFile(encryptFile, decryptFile, TextBox6.Text, TextBox7.Text);
+
+        ArrayList al = new ArrayList();
+
+        al = func.FileToArray(Server.MapPath(".") + "\\c.txt");
+        TextBox8.Text = al[0].ToString();
     }
 }
